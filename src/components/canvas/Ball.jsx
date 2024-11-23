89f81855b302206dab791c2849147af1b28e54a1
saimim <saimimislam@gmail.com>
2024-11-23 20:51:59 +0600

    Simplify for mobilephoe

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -13,39 +13,62 @@ import CanvasLoader from "../Loader";
 const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
+  const isMobile = window.innerWidth <= 768;
+
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0, 0.05]} />
-      <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
-        <meshStandardMaterial
-          color='#fff8eb'
-          polygonOffset
-          polygonOffsetFactor={-5}
-          flatShading
-        />
-        <Decal
-          position={[0, 0, 1]}
-          rotation={[2 * Math.PI, 0, 6.25]}
-          scale={1}
-          map={decal}
-          flatShading
-        />
-      </mesh>
-    </Float>
+    <React.Fragment>
+      {isMobile ? (
+        <mesh castShadow receiveShadow scale={2.75}>
+          <icosahedronGeometry args={[1, 0]} />
+          <meshStandardMaterial
+            color="#8B4513" // Changed to brown
+            polygonOffset
+            polygonOffsetFactor={-5}
+            flatShading
+          />
+          <Decal
+            position={[0, 0, 1]}
+            rotation={[2 * Math.PI, 0, 6.25]}
+            scale={1}
+            map={decal}
+            flatShading
+          />
+        </mesh>
+      ) : (
+        <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+          <mesh castShadow receiveShadow scale={2.75}>
+            <icosahedronGeometry args={[1, 1]} />
+            <meshStandardMaterial
+              color="#8B4513" // Changed to brown
+              polygonOffset
+              polygonOffsetFactor={-5}
+              flatShading
+            />
+            <Decal
+              position={[0, 0, 1]}
+              rotation={[2 * Math.PI, 0, 6.25]}
+              scale={1}
+              map={decal}
+              flatShading
+            />
+          </mesh>
+        </Float>
+      )}
+    </React.Fragment>
   );
 };
 
 const BallCanvas = ({ icon }) => {
+  const isMobile = window.innerWidth <= 768;
+
   return (
     <Canvas
-      frameloop='demand'
+      frameloop={isMobile ? "demand" : "always"}
       dpr={[1, 2]}
-      gl={{ preserveDrawingBuffer: true }}
+      gl={{ preserveDrawingBuffer: !isMobile }}
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls enableZoom={false} />
+        <OrbitControls enableZoom={false} enablePan={!isMobile} />
         <Ball imgUrl={icon} />
       </Suspense>
 
--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,25 +1,27 @@
-import { useState, useRef, Suspense } from "react";
+import { useRef, useState, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
-import * as random from "maath/random/dist/maath-random.esm";
+import { random } from "maath";
 
-const Stars = (props) => {
+const Stars = () => {
   const ref = useRef();
-  const [sphere] = useState(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }));
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.2 })
+  );
 
-  useFrame((state, delta) => {
+  useFrame((_, delta) => {
     ref.current.rotation.x -= delta / 10;
     ref.current.rotation.y -= delta / 15;
   });
 
   return (
     <group rotation={[0, 0, Math.PI / 4]}>
-      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled>
         <PointMaterial
           transparent
-          color='#f272c8'
+          color="#f272c8"
           size={0.002}
-          sizeAttenuation={true}
+          sizeAttenuation
           depthWrite={false}
         />
       </Points>
@@ -29,12 +31,11 @@ const Stars = (props) => {
 
 const StarsCanvas = () => {
   return (
-    <div className='w-full h-auto absolute inset-0 z-[-1]'>
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
       <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
         </Suspense>
-
         <Preload all />
       </Canvas>
     </div>
